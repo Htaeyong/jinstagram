5f78065ca9bb2f6ec83f2e13006f36bc41d22407
Htaeyong <33157601+Htaeyong@users.noreply.github.com>
2022-07-31 14:11:24 +0900

    Update settings.py

--- jinstagram/jinstagram/settings.py
+++ jinstagram/jinstagram/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '../.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n2o3(72jgy+k=&*@88uhcztbw4iajenu3v*lnhb5ni7m9f3&c9'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
