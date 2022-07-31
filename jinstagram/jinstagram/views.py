967788260ece73e9fa74202650ee5f195cb69947
Taeyong <cap577@naver.com>
2022-07-31 14:07:36 +0900

    Initial commit

---
+++ jinstagram/jinstagram/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class Sub(APIView):
+    def get(self, request):
+        print("GET 호출")
+        return render(request, "main.html")
+
+    def post(self, request):
+        print("POST 호출")
+        return render(request, "main.html")
